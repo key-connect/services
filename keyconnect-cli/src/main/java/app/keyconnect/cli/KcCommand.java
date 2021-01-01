1127e13d44e14466320d061e50a64d2f34e047ab
Manthan Dave <manthanhd@live.com>
2021-01-01 19:24:49 +0000

    Enable help mixin options

--- keyconnect-cli/src/main/java/app/keyconnect/cli/KcCommand.java
+++ keyconnect-cli/src/main/java/app/keyconnect/cli/KcCommand.java
@@ -2,7 +2,7 @@ package app.keyconnect.cli;
 
 import picocli.CommandLine.Command;
 
-@Command(name = "kc", description = "Key Connect command line interface")
+@Command(name = "kc", description = "Key Connect command line interface", mixinStandardHelpOptions = true)
 public class KcCommand {
 
 }
