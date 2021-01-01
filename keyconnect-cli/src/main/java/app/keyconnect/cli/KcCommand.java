32830e75ecf09f082876c6b6568f70268d5076ce
Manthan Dave <manthanhd@live.com>
2021-01-01 19:27:23 +0000

    Formatting

--- keyconnect-cli/src/main/java/app/keyconnect/cli/KcCommand.java
+++ keyconnect-cli/src/main/java/app/keyconnect/cli/KcCommand.java
@@ -2,7 +2,9 @@ package app.keyconnect.cli;
 
 import picocli.CommandLine.Command;
 
-@Command(name = "kc", description = "Key Connect command line interface", mixinStandardHelpOptions = true)
+@Command(name = "kc",
+    description = "Key Connect command line interface",
+    mixinStandardHelpOptions = true)
 public class KcCommand {
 
 }
