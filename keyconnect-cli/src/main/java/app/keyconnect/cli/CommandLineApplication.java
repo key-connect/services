4ff2e401458ddb25d07d64ad5a607ca10abefb35
Manthan Dave <manthanhd@live.com>
2021-01-01 19:24:01 +0000

    Add windows colours support + enable default colour scheme

--- keyconnect-cli/src/main/java/app/keyconnect/cli/CommandLineApplication.java
+++ keyconnect-cli/src/main/java/app/keyconnect/cli/CommandLineApplication.java
@@ -10,12 +10,18 @@ import app.keyconnect.cli.commands.ServerStatusCommand;
 import app.keyconnect.cli.commands.TransactionCommand;
 import com.google.gson.Gson;
 import java.net.ConnectException;
+import org.fusesource.jansi.AnsiConsole;
 import picocli.CommandLine;
+import picocli.CommandLine.Help.Ansi;
 
 public class CommandLineApplication {
 
+  public static final int GENERIC_ERROR_CODE = 10;
+  public static final int CONNECTION_ERROR_CODE = 20;
+
   public static void main(String[] args) {
-    new CommandLine(new KcCommand())
+    AnsiConsole.systemInstall();
+    final int exitCode = new CommandLine(new KcCommand())
         .addSubcommand(new ServerStatusCommand())
         .addSubcommand(new BlockchainStatusCommand())
         .addSubcommand(new AccountsCommand())
@@ -25,23 +31,27 @@ public class CommandLineApplication {
         .setExecutionExceptionHandler((ex, commandLine, parseResult) -> {
           if (!(ex instanceof ApiException)) {
             System.out.println(ex);
-            return 10;
+            return GENERIC_ERROR_CODE;
           }
 
           final ApiException apiException = (ApiException) ex;
           final Throwable exceptionCause = apiException.getCause();
           if (exceptionCause instanceof ConnectException) {
             System.out.println(exceptionCause.getMessage());
-            return 20;
+            return CONNECTION_ERROR_CODE;
           }
 
           final String responseBody = apiException.getResponseBody();
           final ExceptionalResponse exceptionalResponse = new Gson()
               .fromJson(responseBody, ExceptionalResponse.class);
-          System.out.println(exceptionalResponse);
-          return 30;
-        })
-        .execute(args);
-  }
 
+          System.out.println(exceptionalResponse);
+
+          return apiException.getCode();
+        })
+        .setColorScheme(CommandLine.Help.defaultColorScheme(Ansi.ON))
+        .execute(args);
+    AnsiConsole.systemUninstall();
+    System.exit(exitCode);
+  }
 }
