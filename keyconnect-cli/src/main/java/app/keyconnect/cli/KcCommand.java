775b1a6dbb56a577cbafb498c1b3423023d6b39d
Manthan Dave <manthanhd@live.com>
2021-01-07 20:38:36 +0000

    Add exit code documentation

--- keyconnect-cli/src/main/java/app/keyconnect/cli/KcCommand.java
+++ keyconnect-cli/src/main/java/app/keyconnect/cli/KcCommand.java
@@ -4,7 +4,14 @@ import picocli.CommandLine.Command;
 
 @Command(name = "kc",
     description = "Key Connect command line interface",
-    mixinStandardHelpOptions = true)
+    mixinStandardHelpOptions = true,
+    exitCodeListHeading = "Exit codes:%n",
+    exitCodeList = {
+        "10:Generic non-api related error, mostly an issue with the CLI or the environment",
+        "20:Network or connection related error",
+        "[HTTP Status Code]:Exit code is the HTTP status code returned by the server"
+    }
+)
 public class KcCommand {
 
 }
