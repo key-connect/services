5d464e214bb91e9aea2756fc954432bfcba7bf6f
Manthan Dave <manthanhd@live.com>
2021-01-21 20:12:34 +0000

    Add wiring for XrpNetworkClientService

--- keyconnect-server/src/main/java/app/keyconnect/server/gateways/XrpGatewayConfiguration.java
+++ keyconnect-server/src/main/java/app/keyconnect/server/gateways/XrpGatewayConfiguration.java
@@ -1,7 +1,10 @@
 package app.keyconnect.server.gateways;
 
+import app.keyconnect.rippled.api.client.PublicRippledClient;
 import app.keyconnect.rippled.api.spring.JacksonConfig;
 import app.keyconnect.server.factories.configuration.YamlConfiguration;
+import app.keyconnect.server.services.networks.NetworkClientService;
+import app.keyconnect.server.services.networks.XrpNetworkClientService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -11,7 +14,18 @@ import org.springframework.http.converter.json.MappingJackson2HttpMessageConvert
 @Import({JacksonConfig.class, YamlConfiguration.class})
 public class XrpGatewayConfiguration {
 
-  @Bean("XrpGateway")
+  private static final String BEAN_XRP_NETWORK_SERVICE = "xrpNetworkService";
+  private static final String XRP_GATEWAY = "XrpGateway";
+
+  @Bean(BEAN_XRP_NETWORK_SERVICE)
+  public NetworkClientService<PublicRippledClient> xrpNetworkClientService(
+      YamlConfiguration yamlConfiguration,
+      MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter) {
+    return new XrpNetworkClientService(yamlConfiguration,
+        () -> JacksonConfig.constructRestTemplate(mappingJackson2HttpMessageConverter));
+  }
+
+  @Bean(XRP_GATEWAY)
   public XrpGateway xrpGateway(YamlConfiguration configuration,
       MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter) {
     return new XrpGateway(configuration,
