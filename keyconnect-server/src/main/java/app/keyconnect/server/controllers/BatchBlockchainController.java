ce9673bb809fcc58bb09929bcd736b5aed3ed0ec
Manthan Dave <manthanhd@live.com>
2021-01-23 17:26:29 +0000

    Implement batch endpoint

---
+++ keyconnect-server/src/main/java/app/keyconnect/server/controllers/BatchBlockchainController.java
@@ -0,0 +1,62 @@
+package app.keyconnect.server.controllers;
+
+import app.keyconnect.api.client.model.AccountsInfoRequest;
+import app.keyconnect.api.client.model.AccountsInfoResponse;
+import app.keyconnect.api.client.model.BlockchainAccountInfo;
+import app.keyconnect.server.factories.BlockchainGatewayFactory;
+import app.keyconnect.server.gateways.BlockchainGateway;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BatchBlockchainController {
+
+  private final BlockchainGatewayFactory blockchainGatewayFactory;
+  private final ExecutorService workPool = Executors.newWorkStealingPool(32);
+
+  @Autowired
+  public BatchBlockchainController(
+      BlockchainGatewayFactory blockchainGatewayFactory) {
+    this.blockchainGatewayFactory = blockchainGatewayFactory;
+  }
+
+  @PostMapping(
+      path = "/v1/batch/blockchains/accounts/info",
+      produces = MediaType.APPLICATION_JSON_VALUE
+  )
+  public ResponseEntity<AccountsInfoResponse> getBlockchainAccountInfo(
+      @RequestBody AccountsInfoRequest accountsInfoRequest,
+      @RequestParam(value = "network", required = false, defaultValue = "mainnet") String network
+  ) {
+    final List<BlockchainAccountInfo> blockchainAccountInfoList = accountsInfoRequest.getAccounts()
+        .stream()
+        .map(request -> workPool.submit(() -> {
+          final BlockchainGateway gateway = blockchainGatewayFactory
+              .getGateway(request.getChainId().getValue());
+          return gateway.getAccount(network, request.getAccountId());
+        }))
+        .map(c -> {
+          try {
+            return c.get();
+          } catch (InterruptedException | ExecutionException e) {
+            // ignore
+            return new BlockchainAccountInfo(); // fill in error
+          }
+        })
+        .collect(Collectors.toList());
+    return ResponseEntity.ok(
+        new AccountsInfoResponse()
+            .accounts(blockchainAccountInfoList)
+    );
+  }
+}
--- keyconnect-server/src/main/java/app/keyconnect/server/controllers/BlockchainAccountController.java
+++ keyconnect-server/src/main/java/app/keyconnect/server/controllers/BlockchainAccountController.java
@@ -1,5 +1,7 @@
 package app.keyconnect.server.controllers;
 
+import app.keyconnect.api.client.model.AccountsInfoRequest;
+import app.keyconnect.api.client.model.AccountsInfoResponse;
 import app.keyconnect.server.gateways.exceptions.UnknownNetworkException;
 import app.keyconnect.api.client.model.BlockchainAccountInfo;
 import app.keyconnect.api.client.model.BlockchainAccountPayments;
@@ -10,6 +12,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
