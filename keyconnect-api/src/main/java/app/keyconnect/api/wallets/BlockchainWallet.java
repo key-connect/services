5d1d7c8670c2e5cb9b3b51364d792d337d77f83a
Manthan Dave <manthanhd@live.com>
2021-02-02 07:59:22 +0000

    Add javadoc

--- keyconnect-api/src/main/java/app/keyconnect/api/wallets/BlockchainWallet.java
+++ keyconnect-api/src/main/java/app/keyconnect/api/wallets/BlockchainWallet.java
@@ -1,12 +1,21 @@
 package app.keyconnect.api.wallets;
 
-import java.io.File;
-
 public interface BlockchainWallet {
 
+  /**
+   * Funds the wallet. This method should only be operable in test environments.
+   */
   void fundWallet();
 
+  /**
+   * Gets the wallet generation seed.
+   * @return Wallet generation seed.
+   */
   String getSeed();
 
+  /**
+   * Gets the wallet public address.
+   * @return Wallet public address.
+   */
   String getAddress();
 }
--- keyconnect-api/src/main/java/app/keyconnect/api/wallets/BlockchainWalletFactory.java
+++ keyconnect-api/src/main/java/app/keyconnect/api/wallets/BlockchainWalletFactory.java
@@ -4,6 +4,11 @@ import app.keyconnect.api.client.model.BlockchainAccountInfo.ChainIdEnum;
 
 public interface BlockchainWalletFactory {
 
+  /**
+   * Generates a blockchain wallet given a blockchain ID
+   * @param chainId Blockchain ID, one of ETH | XRP.
+   * @return New blockchain wallet
+   */
   BlockchainWallet generate(ChainIdEnum chainId);
 
 }
