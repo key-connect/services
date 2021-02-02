4574967e36fdf86c82b481702861c51f38a00b50
Manthan Dave <manthanhd@live.com>
2021-02-02 08:21:46 +0000

    Remove funding method from BlockchainWallet

--- keyconnect-api/src/test/java/app/keyconnect/api/wallets/XrpBlockchainWalletTest.java
+++ keyconnect-api/src/test/java/app/keyconnect/api/wallets/XrpBlockchainWalletTest.java
@@ -2,9 +2,6 @@ package app.keyconnect.api.wallets;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import app.keyconnect.api.client.AccountsApi;
-import app.keyconnect.api.client.model.BlockchainAccountInfo;
-import java.math.BigDecimal;
 import org.junit.Test;
 
 public class XrpBlockchainWalletTest {
