cdacff1ffd4d5596cbe1a080d56731fa8832fe94
Manthan Dave <manthanhd@live.com>
2021-02-10 09:00:37 +0000

    Implement wallet encryption

--- keyconnect-api/src/main/java/app/keyconnect/api/wallets/io/WalletReader.java
+++ keyconnect-api/src/main/java/app/keyconnect/api/wallets/io/WalletReader.java
@@ -1,18 +1,32 @@
 package app.keyconnect.api.wallets.io;
 
-import app.keyconnect.api.client.model.BlockchainAccountInfo.ChainIdEnum;
 import app.keyconnect.api.wallets.BlockchainWalletFactory;
 import app.keyconnect.api.wallets.DeterministicWallet;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 import java.util.stream.Collectors;
 import java.util.zip.GZIPInputStream;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import org.apache.commons.codec.binary.Base64;
 
 public class WalletReader {
 
@@ -29,8 +43,10 @@ public class WalletReader {
             .collect(Collectors.joining(System.lineSeparator()));
       }
 
+      final String decryptedWalletString = decrypt(walletString, password);
+
       final WalletFile walletFile = new ObjectMapper(new YAMLFactory())
-          .readValue(walletString, WalletFile.class);
+          .readValue(decryptedWalletString, WalletFile.class);
       final DeterministicWallet wallet = new DeterministicWallet(walletFile.getPassphrase(), walletFile.getMnemonic(),
           System.currentTimeMillis());
       // inflate trees
@@ -52,4 +68,36 @@ public class WalletReader {
     }
   }
 
+  private static String decrypt(String walletString, String password) {
+    if (!walletString.contains(":")) throw new WalletReaderException("Encrypted wallet is malformed or has been tampered with");
+    final String[] encoded = walletString.split(":");
+    if (encoded.length != 3) throw new WalletReaderException("Encrypted wallet missing parameters required for decryption");
+
+    final String saltString = encoded[0];
+    final String ivString = encoded[1];
+    final String cipherTextString = encoded[2];
+
+    try {
+      final byte[] salt = Base64.decodeBase64(saltString);
+      final byte[] iv = Base64.decodeBase64(ivString);
+      final byte[] ciphertext = Base64.decodeBase64(cipherTextString);
+
+      final SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+      final KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 256);
+      final SecretKey tmp = factory.generateSecret(spec);
+      final SecretKey secret = new SecretKeySpec(tmp.getEncoded(), "AES");
+
+      final Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+      cipher.init(Cipher.DECRYPT_MODE, secret, new IvParameterSpec(iv));
+      return new String(cipher.doFinal(ciphertext), StandardCharsets.UTF_8);
+    } catch (NoSuchAlgorithmException
+        | InvalidKeySpecException
+        | NoSuchPaddingException
+        | InvalidKeyException
+        | InvalidAlgorithmParameterException
+        | IllegalBlockSizeException
+        | BadPaddingException e) {
+      throw new WalletReaderException("Error decrypting wallet", e);
+    }
+  }
 }
--- keyconnect-api/src/main/java/app/keyconnect/api/wallets/io/WalletWriter.java
+++ keyconnect-api/src/main/java/app/keyconnect/api/wallets/io/WalletWriter.java
@@ -10,9 +10,26 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.security.AlgorithmParameters;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.InvalidParameterSpecException;
+import java.security.spec.KeySpec;
 import java.util.Optional;
 import java.util.zip.GZIPOutputStream;
 import javax.annotation.Nullable;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import org.apache.commons.codec.binary.Base64;
 
 public class WalletWriter {
 
@@ -26,6 +43,7 @@ public class WalletWriter {
     final Optional<String> optionalEncryptionPassphrase = Optional.ofNullable(encryptionPassphrase);
     verifyFile(file);
     final WalletFile walletFile = new WalletFile(wallet.getMnemonicCode(), wallet.getPassphrase());
+
     wallet.getAllFactories()
         .stream()
         .filter(BlockchainWalletFactory::hasWallets)
@@ -35,14 +53,49 @@ public class WalletWriter {
                 String.valueOf(f.getGeneratedWallets().size())
             )
         );
-    String walletFileString = serializeWalletFileToString(walletFile);
+
+    final String walletFileString = serializeWalletFileToString(walletFile);
+    final byte[] data;
+    data = optionalEncryptionPassphrase
+        .map(s -> encrypt(walletFileString, s).getBytes(StandardCharsets.UTF_8))
+        .orElseGet(() -> walletFileString.getBytes(StandardCharsets.UTF_8));
+
     try {
       final BufferedOutputStream outputStream = new BufferedOutputStream(new GZIPOutputStream(new FileOutputStream(file)));
-      outputStream.write(walletFileString.getBytes(StandardCharsets.UTF_8));
+      outputStream.write(data);
       outputStream.flush();
       outputStream.close();
     } catch (IOException e) {
       throw new WalletWriterException("Unable to write to file", e);
+    }
+  }
+
+  // https://stackoverflow.com/a/992413/929496
+  private String encrypt(String walletFileString, String passphrase) {
+    try {
+      final byte[] saltBytes = new byte[8];
+      new SecureRandom().nextBytes(saltBytes);
+      final SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+      final KeySpec spec = new PBEKeySpec(passphrase.toCharArray(), saltBytes, 65536, 256);
+      final SecretKey tmp = factory.generateSecret(spec);
+      final SecretKey secret = new SecretKeySpec(tmp.getEncoded(), "AES");
+
+      final Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+      cipher.init(Cipher.ENCRYPT_MODE, secret);
+      final AlgorithmParameters params = cipher.getParameters();
+      byte[] iv = params.getParameterSpec(IvParameterSpec.class).getIV();
+      byte[] ciphertext = cipher.doFinal(walletFileString.getBytes(StandardCharsets.UTF_8));
+      return Base64.encodeBase64String(saltBytes)
+          + ":" + Base64.encodeBase64String(iv)
+          + ":" + Base64.encodeBase64String(ciphertext);
+    } catch (NoSuchAlgorithmException
+        | BadPaddingException
+        | IllegalBlockSizeException
+        | InvalidParameterSpecException
+        | InvalidKeyException
+        | NoSuchPaddingException
+        | InvalidKeySpecException e) {
+      throw new WalletWriterException("Problem encrypting wallet", e);
     }
   }
 
