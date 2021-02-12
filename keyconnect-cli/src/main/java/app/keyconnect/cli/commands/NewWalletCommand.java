054dc7f6b89a642adf8fe411aa5077da5f2db3bc
Manthan Dave <manthanhd@live.com>
2021-02-12 12:26:55 +0000

    Check if name is blank

--- keyconnect-cli/src/main/java/app/keyconnect/cli/commands/NewWalletCommand.java
+++ keyconnect-cli/src/main/java/app/keyconnect/cli/commands/NewWalletCommand.java
@@ -5,15 +5,12 @@ import static app.keyconnect.cli.utils.LocalWalletHelper.readLocalWallet;
 import app.keyconnect.api.client.model.BlockchainAccountInfo.ChainIdEnum;
 import app.keyconnect.api.wallets.BlockchainWallet;
 import app.keyconnect.api.wallets.BlockchainWalletFactory;
-import app.keyconnect.api.wallets.DeterministicWallet;
-import app.keyconnect.api.wallets.io.WalletReader;
 import app.keyconnect.api.wallets.io.WalletWriter;
 import app.keyconnect.cli.utils.LocalWalletData;
-import java.io.Console;
-import java.io.File;
 import java.util.Locale;
 import java.util.Optional;
 import java.util.concurrent.Callable;
+import org.apache.commons.lang3.StringUtils;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
@@ -38,16 +35,22 @@ public class NewWalletCommand implements Callable<Integer> {
   @Override
   public Integer call() throws Exception {
     final ChainIdEnum chainId = ChainIdEnum.valueOf(this.chainId.toUpperCase(Locale.ROOT));
+    if (StringUtils.isBlank(name)) {
+      System.out.println("Wallet name cannot be blank");
+      System.exit(1);
+    }
 
     final LocalWalletData localWalletData = readLocalWallet();
-    final BlockchainWalletFactory walletFactory = localWalletData.getWallet().getWalletFactory(chainId);
+    final BlockchainWalletFactory walletFactory = localWalletData.getWallet()
+        .getWalletFactory(chainId);
     final Optional<BlockchainWallet> maybeExistingWallet = walletFactory.getGeneratedWallets()
         .stream()
         .filter(w -> w.getName().equalsIgnoreCase(name))
         .findFirst();
 
     if (maybeExistingWallet.isPresent()) {
-      System.out.println("Wallet already exists with name " + name + ". Please choose a different name for your new wallet.");
+      System.out.println("Wallet already exists with name " + name
+          + ". Please choose a different name for your new wallet.");
       System.exit(1);
     }
 
