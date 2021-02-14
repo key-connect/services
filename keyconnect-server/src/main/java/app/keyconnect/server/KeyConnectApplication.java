d95763889683323ba636ac0030acbe4b5a24ac30
Manthan Dave <manthanhd@live.com>
2021-02-14 16:18:44 +0000

    Enable server-side gzip handling

--- keyconnect-server/src/main/java/app/keyconnect/server/KeyConnectApplication.java
+++ keyconnect-server/src/main/java/app/keyconnect/server/KeyConnectApplication.java
@@ -1,8 +1,12 @@
 package app.keyconnect.server;
 
+import org.eclipse.jetty.server.handler.HandlerCollection;
+import org.eclipse.jetty.server.handler.gzip.GzipHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableCaching
@@ -10,6 +14,20 @@ public class KeyConnectApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(KeyConnectApplication.class, args);
+  }
+
+  @Bean
+  public JettyServletWebServerFactory jettyServletWebServerFactory() {
+    JettyServletWebServerFactory factory = new JettyServletWebServerFactory();
+    factory.addServerCustomizers(server -> {
+      GzipHandler gzipHandler = new GzipHandler();
+      gzipHandler.setInflateBufferSize(1);
+      gzipHandler.setHandler(server.getHandler());
+
+      HandlerCollection handlerCollection = new HandlerCollection(gzipHandler);
+      server.setHandler(handlerCollection);
+    });
+    return factory;
   }
 
 }
