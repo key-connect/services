d2bae989dd1cc3d08826d41f89343d80395acfaf
Manthan Dave <manthanhd@live.com>
2021-02-24 08:46:23 +0000

    Implement standalone EthWallet

--- keyconnect-sdk/src/main/java/app/keyconnect/sdk/wallets/EthWallet.java
+++ keyconnect-sdk/src/main/java/app/keyconnect/sdk/wallets/EthWallet.java
@@ -4,7 +4,15 @@ import app.keyconnect.api.client.model.BlockchainAccountInfo.ChainIdEnum;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.security.SecureRandom;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import org.apache.commons.lang3.StringUtils;
+import org.bitcoinj.crypto.DeterministicKey;
+import org.bitcoinj.crypto.HDUtils;
+import org.bitcoinj.wallet.DeterministicKeyChain;
+import org.bitcoinj.wallet.DeterministicSeed;
 import org.jetbrains.annotations.Nullable;
+import org.web3j.crypto.Bip39Wallet;
 import org.web3j.crypto.Credentials;
 import org.web3j.crypto.RawTransaction;
 import org.web3j.crypto.TransactionEncoder;
@@ -14,16 +22,31 @@ public class EthWallet implements BlockchainWallet {
 
   public static final BigDecimal ETH_SCALE = BigDecimal.valueOf(1000000000000000000L);
   private final Credentials credentials;
+  private final DeterministicSeed seed;
+  private final DeterministicKeyChain chain;
+  private final String passphrase;
   private String name;
 
   public EthWallet(String name, BigInteger privateKey) {
     this.name = name;
+    this.seed = null;
+    this.chain = null;
+    this.passphrase = null;
     this.credentials = Credentials.create(privateKey.toString(16));
   }
 
-  public EthWallet(String name) {
+  public EthWallet(String name, String passphrase) {
     this.name = name;
-    this.credentials =
+    this.seed = new DeterministicSeed(
+        new SecureRandom(),
+        256,
+        Optional.ofNullable(passphrase).orElse("")
+    );
+    this.passphrase = passphrase;
+    this.chain = DeterministicKeyChain.builder().seed(seed).build();
+    final DeterministicKey key = chain
+        .getKeyByPath(HDUtils.parsePath("M/44H/60H/0H/0/0"), true);
+    this.credentials = Credentials.create(key.getPrivKey().toString(16));
   }
 
   private String sign(RawTransaction tx) {
@@ -56,6 +79,20 @@ public class EthWallet implements BlockchainWallet {
   @Override
   public void setName(String name) {
     this.name = name;
+  }
+
+  @Nullable
+  public String getPassphrase() {
+    return passphrase;
+  }
+
+  @Nullable
+  public String getMnemonic() {
+    if (null != seed && null != seed.getMnemonicCode()) {
+      return String.join(" ", seed.getMnemonicCode());
+    }
+
+    return null;
   }
 
   @Override
