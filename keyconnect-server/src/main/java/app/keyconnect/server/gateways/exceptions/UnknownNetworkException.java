24acb43704d8065373f029177de4d921c73dbcf4
Manthan Dave <manthanhd@live.com>
2021-03-05 08:25:26 +0000

    Update exceptions to be more helpful

--- keyconnect-server/src/main/java/app/keyconnect/server/gateways/exceptions/UnknownNetworkException.java
+++ keyconnect-server/src/main/java/app/keyconnect/server/gateways/exceptions/UnknownNetworkException.java
@@ -2,11 +2,12 @@ package app.keyconnect.server.gateways.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 @ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Specified network is not a valid network for the block chain")
-public class UnknownNetworkException extends Exception {
+public class UnknownNetworkException extends ResponseStatusException {
 
   public UnknownNetworkException(String chainId, String specifiedNetwork) {
-    super(specifiedNetwork + " is not a known network for blockchain " + chainId);
+    super(HttpStatus.BAD_REQUEST, specifiedNetwork + " is not a known network for blockchain " + chainId);
   }
 }
